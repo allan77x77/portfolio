85fff93385e5ce5df3ab7774ca359d54c3b087dd
Allan77 <ranto.allan7@gmail.com>
2025-01-07 17:02:18 +0300

    change app.js

--- src/App.js
+++ src/App.js
@@ -29,18 +29,17 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/portfolio">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
-        <Router basename="/portfolio/"></Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
